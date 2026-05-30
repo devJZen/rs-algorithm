6c8cf8b12e812ab8e988a926643f8316e7088bf5
devJZen <quasar.joo@gmail.com>
2026-05-31 01:30:22 +0900

    update: for onboarding

--- README.md
+++ README.md
@@ -3,5 +3,41 @@
 ## spec 
  
 ```bash 
-cargo run -- -a bubble -n 20 
+cargo run
 ``` 
+
+## Why Made For?
+
+I study algorithm but hard to understand sorting for arrangement so made first architect and frame with clap, tmux, claude.
+Futher more want to build ASCII art for understood algorithm. want to Change re-build new artwork and frame.
+
+## Example
+
+```
+bubble.rs
+selection.rs
+insertion.rs
+```
+another sorting algorithms and data structures need contributing.
+
+> [Goals] try to no use claude
+
+fail is okay just contributed and study lots of example.
+i usually solve for algorithm with Map and HashMap thesedays but i need expanding my range of thinking.
+
+## Reference
+
+Inspired by [ratatui's tutorials](https://ratatui.rs/tutorials/videos/)
+and [ratatui's examples](https://ratatui.rs/examples/apps/)
+
+Sorting and Searching Wiki
+
+[Bubble_sort](https://en.wikipedia.org/wiki/Bubble_sort)
+...
+
+Study Data Structures
+
+[Data_structure](https://en.wikipedia.org/wiki/Data_structure)
+[Abstract_data_type](https://en.wikipedia.org/wiki/Abstract_data_type)
+[Algorithm-Repository](https://github.com/woorimlee/Algorithm-Repository/tree/master)
+...
--- clapping/target/debug/deps/clapping-0fdbfa6a2079b8c8.d
+++ clapping/target/debug/deps/clapping-0fdbfa6a2079b8c8.d
@@ -1,5 +1,31 @@
-C:\Users\USER\Desktop\study\clapping\target\debug\deps\clapping-0fdbfa6a2079b8c8.d: src\main.rs
+C:\Users\USER\Desktop\study\clapping\target\debug\deps\clapping-0fdbfa6a2079b8c8.d: src\main.rs src\action.rs src\app.rs src\cli.rs src\components\mod.rs src\components\algorithms\mod.rs src\components\algorithms\bubble.rs src\components\algorithms\heap.rs src\components\algorithms\insertion.rs src\components\algorithms\intro.rs src\components\algorithms\merge.rs src\components\algorithms\quick.rs src\components\algorithms\selection.rs src\components\algorithms\shell.rs src\components\algorithms\tim.rs src\components\data_structures\mod.rs src\components\data_structures\array.rs src\components\data_structures\binary_tree.rs src\components\data_structures\bst.rs src\components\data_structures\graph.rs src\components\data_structures\hash_table.rs src\components\data_structures\heap.rs src\components\data_structures\linked_list.rs src\components\data_structures\queue.rs src\components\data_structures\stack.rs src\components\home.rs src\tui.rs
 
-C:\Users\USER\Desktop\study\clapping\target\debug\deps\libclapping-0fdbfa6a2079b8c8.rmeta: src\main.rs
+C:\Users\USER\Desktop\study\clapping\target\debug\deps\libclapping-0fdbfa6a2079b8c8.rmeta: src\main.rs src\action.rs src\app.rs src\cli.rs src\components\mod.rs src\components\algorithms\mod.rs src\components\algorithms\bubble.rs src\components\algorithms\heap.rs src\components\algorithms\insertion.rs src\components\algorithms\intro.rs src\components\algorithms\merge.rs src\components\algorithms\quick.rs src\components\algorithms\selection.rs src\components\algorithms\shell.rs src\components\algorithms\tim.rs src\components\data_structures\mod.rs src\components\data_structures\array.rs src\components\data_structures\binary_tree.rs src\components\data_structures\bst.rs src\components\data_structures\graph.rs src\components\data_structures\hash_table.rs src\components\data_structures\heap.rs src\components\data_structures\linked_list.rs src\components\data_structures\queue.rs src\components\data_structures\stack.rs src\components\home.rs src\tui.rs
 
 src\main.rs:
+src\action.rs:
+src\app.rs:
+src\cli.rs:
+src\components\mod.rs:
+src\components\algorithms\mod.rs:
+src\components\algorithms\bubble.rs:
+src\components\algorithms\heap.rs:
+src\components\algorithms\insertion.rs:
+src\components\algorithms\intro.rs:
+src\components\algorithms\merge.rs:
+src\components\algorithms\quick.rs:
+src\components\algorithms\selection.rs:
+src\components\algorithms\shell.rs:
+src\components\algorithms\tim.rs:
+src\components\data_structures\mod.rs:
+src\components\data_structures\array.rs:
+src\components\data_structures\binary_tree.rs:
+src\components\data_structures\bst.rs:
+src\components\data_structures\graph.rs:
+src\components\data_structures\hash_table.rs:
+src\components\data_structures\heap.rs:
+src\components\data_structures\linked_list.rs:
+src\components\data_structures\queue.rs:
+src\components\data_structures\stack.rs:
+src\components\home.rs:
+src\tui.rs:
